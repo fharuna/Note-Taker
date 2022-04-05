488c97e335389794c80516ef0990bb0d4d54e980
Fati Haruna <haruna.fati@gmail.com>
2022-04-04 23:23:40 -0400

    js addition

---
+++ routes/api-routes.js
@@ -0,0 +1,60 @@
+var activeNote = {};
+
+// to get notes from the database
+var getNotes = function() {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET"
+  });
+};
+
+// to save notes to the database
+var saveNote = function(note) {
+  return $.ajax({
+    url: "/api/notes",
+    data: note,
+    method: "POST"
+  });
+};
+
+// to delete notes from the database
+var deleteNote = function(id) {
+    return $.ajax({
+      url: "api/notes" + id,
+      method: "DELETE"
+    });
+  };
+
+  var renderActiveNote = function() {
+    $saveNoteBtn.hide();
+  
+    if (activeNote.id) {
+      $noteTitle.attr("readonly", true);
+      $noteText.attr("readonly", true);
+      $noteTitle.val(activeNote.title);
+      $noteText.val(activeNote.text);
+    } else {
+      $noteTitle.attr("readonly", false);
+      $noteText.attr("readonly", false);
+      $noteTitle.val("");
+      $noteText.val("");
+    }
+  };
+
+  var handleNoteDelete = function(event) {
+    event.stopPropagation();
+  
+    var note = $(this)
+      .parent(".list-group-item")
+      .data();
+  
+    if (activeNote.id === note.id) {
+      activeNote = {};
+    }
+  
+    deleteNote(note.id).then(function() {
+      getAndRenderNotes();
+      renderActiveNote();
+    });
+  };
+
