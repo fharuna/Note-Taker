81896c0df3b8fe6cc83f19cd0c545a74ddb47eed
Fati Haruna <haruna.fati@gmail.com>
2022-04-04 22:48:10 -0400

    adding to assets folder

---
+++ public/assets/index.js
@@ -0,0 +1,5 @@
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
